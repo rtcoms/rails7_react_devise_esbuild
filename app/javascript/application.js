97cc397dfb09f47115a3cfc6bc9a6a13ee8cf8ed
Rohit Trivedi <86.rohit@gmail.com>
2022-06-27 04:37:21 +0530

    user AuthContext to render app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,10 @@
 // Entry point for the build script in your package.json
 
 import App from './components/app'
+import {AuthContextProvider} from "./AuthContext";
 import {createRoot} from "react-dom/client";
 import * as React from "react";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App tab="home" />);
+root.render(<AuthContextProvider><App tab="home" /></AuthContextProvider>);
